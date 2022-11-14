c8e6ccec4e81d8c71f0992f906a56d3ae09c97a6
Nastykiulo <nastya_kiulo@mail.ru>
2022-11-14 16:49:13 +0300

    Test #3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[ show edit update destroy ]
 
-  # GET /questions or /questions.json
   def index
     @questions = Question.all
   end
